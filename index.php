cd02e5e5524cc5df61b1ceec0b6cff28518d3ec1
Malhotraq <93586099+Malhotraq@users.noreply.github.com>
2022-05-24 08:39:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $is_production = false;
 
 $api_url = $is_production ? 
   'https://app.midtrans.com/snap/v1/transactions' : 
-  'https://app.sandbox.midtrans.com/snap/v1/transactions';
+  'https://app.sandbox.midtrans.com/snap/v2/transactions';
 
 
 if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
@@ -37,18 +37,17 @@ function chargeAPI($api_url, $server_key, $request_body){
     CURLOPT_POST => 1,
     CURLOPT_HEADER => 0,
 
-
     CURLOPT_HTTPHEADER => array(
-      'Content-Type: application/json',
-      'Accept: application/json',
-      'Authorization: Basic ' . base64_encode($server_key . ':')
+      "Content-Type: application/json",
+      "Accept: application/json'",
+      "Authorization: Basic " . base64_encode($server_key . ':')
     ),
     CURLOPT_POSTFIELDS => $request_body
   );
   curl_setopt_array($ch, $curl_options);
   $result = array(
     'body' => curl_exec($ch),
-    'http_code' => curl_getinfo($ch, CURLINFO_HTTP_CODE),
+    'http_code' => curl_getinfo($ch, CURLINFO_HTTP_CODE)
   );
   return $result;
 }
